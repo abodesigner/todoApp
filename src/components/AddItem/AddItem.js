54dae154a2be55069aa4dea9eaf6f2c96e53cb80
unknown <mohammed.abosalim@gmail.com>
2019-02-20 14:37:09 +0200

    add components

---
+++ src/components/AddItem/AddItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './AddItem.css';
+class AddItem extends Component {
+  render() {
+    return (
+      <div>
+        <h4 style={{color:'green'}}>Add Item</h4>
+      </div>
+    )
+  }
+}
+
+export default AddItem;
---
+++ src/components/TodoItems/TodoItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoItems.css';
+
+ const TodoItems = () => {
+  return (
+    <div>
+      <h1 style={{color:'red'}}>TodoItems</h1>
+    </div>
+  )
+}
+
+export default TodoItems;
